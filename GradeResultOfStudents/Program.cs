d33292637d54f7b0eb3c17255bb1189921aa6df5
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-17 11:57:25 +0100

    Add project files.

---
+++ GradeResultOfStudents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeResultOfStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentPoint = 65;
+            int minimumSuccessfulPoint = 60;
+            if (studentPoint<minimumSuccessfulPoint)
+            
+                Console.WriteLine("Unsuccessful");
+            else
+                Console.WriteLine("Successful");
+            Console.ReadLine();
+        }
+    }
+}
