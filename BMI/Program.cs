e5e0a47841d8b6a83b3f438494379f6894dac60e
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-21 14:12:25 +0100

    Bmi

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -11,6 +11,41 @@ namespace BMI
         static void Main(string[] args)
         {
 
+            /*
+                BMI < 18.5 Underweight
+                18.5 ≤ BMI < 25.0 Normal
+                25.0 ≤ BMI < 30.0 Overweight
+                30.0 ≤ BMI Obese
+             */
+            Console.Write("Enter your weight in kg :");
+            double weight;
+            Double.TryParse(Console.ReadLine(), out weight);
+
+            Console.Write("Enter height in meters: ");
+            double height;
+            Double.TryParse(Console.ReadLine(), out height);
+
+            double bmi = weight / (height * height);
+
+            Console.WriteLine("BMI: " + Math.Round(bmi, 2));
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("Underweight");
+            }
+            else if (bmi < 25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (bmi < 30)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else if(bmi>=30)
+            {
+                Console.WriteLine("Obese");
+            }
+            Console.ReadLine();
+
         }
     }
 }
