59381b52fe72f5b6fa004f0317e45551dd2762e9
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-22 17:27:52 +0100

    LoginProject

---
+++ LoginProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passwordTry = 3;
+            while (true)
+            {
+                Console.Write("Please enter your user name: ");
+                string name =Console.ReadLine();
+                Console.Write("Please enter your password:");
+                string password =Console.ReadLine();
+
+
+                if (name =="Merve" && password == "12345")
+                {
+                    Console.WriteLine("Congratulations! Login successful!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter correct name and password.");
+                    if (passwordTry>0)
+                    passwordTry -= 1;
+                    if (passwordTry==0)
+                {
+                    Console.WriteLine("You tried 3 times with wrong name or password.");
+                        break;
+                }
+                }
+
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
