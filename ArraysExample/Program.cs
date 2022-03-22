2ff7c3e2de4ff9b04ab39a79532ae60ae08d9ca2
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-22 19:09:31 +0100

    Arrays Example

---
+++ ArraysExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[4];
+            //numbers[0] = 1;
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+            //numbers[3] = 4;
+            //for (int i = 0; i < numbers.Length; i++)
+            //    Console.WriteLine(numbers[i]);
+            string[] names = new string[5];
+            names[0] = "Merve";
+            names[1] = "Derin";
+            names[2]= "Deniz";
+            names[3] = "Matu";
+            names[4] = "Findik";
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
