9b4c19cf6007373b1075ff6b3e22e94d31624cfe
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-29 21:36:50 +0200

    Foreach For Arrays

---
+++ ForeachForArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachForArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Merve", "Derin", "Deniz", "Matu" };
+            //for (int i = 0; i <names.Length ; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
