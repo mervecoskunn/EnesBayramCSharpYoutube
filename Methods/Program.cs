2d7929b3c4f218182542df7bc9735fe24a40b57d
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-30 10:44:37 +0200

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            name();
+            name();
+            name();
+            name();
+            name();
+            surname();
+            surname();
+            surname();
+            surname();
+            surname();
+            Console.ReadLine();
+
+
+        }
+        static void name()
+        {
+            Console.WriteLine("Merve");
+           
+        }
+        static void surname()
+        {
+            Console.WriteLine("Coskun");
+           
+        }
+
+        
+    }
+    
+}
