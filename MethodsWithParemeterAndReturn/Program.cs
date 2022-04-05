c889a61f24143b12240dd79bd7ebdb2582959182
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-05 10:27:30 +0200

    Methods With Paremeter And Return

---
+++ MethodsWithParemeterAndReturn/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithParemeterAndReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string newNameAndSurname = NameAndSurname("Merve","Coskun");
+            Console.WriteLine(newNameAndSurname);
+            Console.ReadLine();
+        }
+        static string NameAndSurname(string name, string surname)
+        {
+              return name +" "+surname;
+        }
+    }
+}
