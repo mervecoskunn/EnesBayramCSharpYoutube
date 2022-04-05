baea2bb1a20739bb96cee34e2eb8b8132e9f1d57
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-05 10:14:01 +0200

    Return wıth parameter

---
+++ MethodsReturn/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Return wıth parameter
+             * 
+             */
+            int newResult = addition(6,7);
+            if (newResult > 10)
+                Console.WriteLine("New result is bigger than result. "+"\nNew Result is : " + newResult);
+
+            else
+                Console.WriteLine("New result is smaller than result."+"\nNew Result is : " + newResult);
+            Console.ReadLine();
+           
+        }
+        static int addition(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+            
+
+        }
+    }
+}
