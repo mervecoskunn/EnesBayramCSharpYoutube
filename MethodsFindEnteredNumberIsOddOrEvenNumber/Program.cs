5d75e3456a226043b20a4a399e4824a03e9e7eef
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-05 10:49:43 +0200

    Methods Find Entered Number Is Odd Or Even Number

---
+++ MethodsFindEnteredNumberIsOddOrEvenNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsFindEnteredNumberIsOddOrEvenNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* if entered number is odd; write this number is odd 
+             * if entered number is even; write this number is even number.
+             */
+            Console.Write("Enter a number: ");
+           int.TryParse(Console.ReadLine(), out var number);
+            IsOddOrEven(number);
+            Console.ReadLine();
+
+
+        }
+        static void IsOddOrEven(int number)
+        {
+            if (number%2== 0)
+                Console.WriteLine("This number is  an even number.");
+            else
+                Console.WriteLine("This number is an odd number.");
+           
+
+        } 
+        
+    }
+}
