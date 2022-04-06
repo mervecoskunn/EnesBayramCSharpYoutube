73140967c1ecaaea09ae450761022561f19f8c99
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-06 10:17:49 +0200

    Class

---
+++ Class/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Car
+    {
+        public int carDoorCounter;
+        public string carModel ;
+        public string carColor;
+
+
+
+        public void startCar()
+        {
+            Console.WriteLine("Car running...");
+
+        }
+        public void lockTheDoors()
+        {
+            Console.WriteLine("Doors are locking...");
+        }
+
+
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.carDoorCounter = 4;
+            car1.carColor = "Red";
+            car1.carModel = "Tesla";
+            car1.lockTheDoors();
+            car1.startCar();
+            Console.WriteLine("Counter car of doors: "+ car1.carDoorCounter);
+            Console.WriteLine("Color of car: "+car1.carColor);
+            Console.WriteLine("Model of car: "+ car1.carModel);
+            Console.ReadLine();
+            
+        }
+    }
+}
